a1683c05521e50895878b2332d0daaa918983fb1
August Fu <yuntongf@gmail.com>
2023-07-08 14:56:27 -0400

    if else parser

--- ast.cpp
+++ ast.cpp
@@ -1,156 +1,93 @@
-#include"lexer.cpp"
-#include<iostream>
-#include<memory>
-#include<vector>
+#include"ast.h"
 
 // turn all fields into pointers
 
 using namespace std;
 
-class Node {
-    public:
-    virtual string serialize() const = 0;
-};
-
 /************************* Expressions *********************/
-class Expression : public Node {
-    public:
-    Expression() = default;
-    virtual ~Expression() = default;
-    virtual string serialize() const {return "";};
+Expression::Expression() = default;
+Expression::~Expression() = default;
+string Expression::serialize() const {return "";}
+
+string Identifier::serialize() const {return value;}
+IntLiteral::IntLiteral(Token tok, int val) : token(tok), value(val) {};
+string IntLiteral::serialize() const {return token.literal;};
+
+BoolLiteral::BoolLiteral(Token tok, bool val) : token(tok), value(val){};
+string BoolLiteral::serialize() const {return token.literal;}
+
+PrefixExpression::PrefixExpression() = default;
+PrefixExpression::PrefixExpression(Token tok, string Operator, unique_ptr<Expression>& right) : token(tok), Operator(Operator), right(move(right)) {};
+string PrefixExpression::serialize() const {
+    return Operator + right.get()->serialize();
+}
+
+InfixExpression::InfixExpression() = default;
+InfixExpression::InfixExpression(Token tok, string Operator, unique_ptr<Expression>& left, unique_ptr<Expression>& right) : token(tok), Operator(Operator), left(move(left)), right(move(right)) {};
+string InfixExpression::serialize() const {
+    string res =  "(" + left.get()->serialize() + " " + Operator + " " + right.get()->serialize() + ")";
+    return res;
 };
-class Identifier : public Expression {
-    public:
-    Token token;
-    string value;
-    string serialize() const final override {
-        return value;
-    }
-};
-class IntLiteral : public Expression {
-    public:
-    Token token;
-    int value;
 
-    IntLiteral(Token tok, int val) : token(tok), value(val) {};
+IfExpression::IfExpression(Token tok, 
+unique_ptr<Expression>& cond, 
+unique_ptr<BlockStatement>& cons, 
+unique_ptr<BlockStatement>& alt) : token(tok), condition(move(cond)), consequence(move(cons)), alternative(move(alt)){};
 
-    string serialize() const final override {
-        return token.literal;
-    }
-};
-class BoolLiteral : public Expression {
-    public:
-    Token token;
-    bool value;
+string IfExpression::serialize() const {
+    string alt;
+    if (alternative != nullptr) alt = " else " + alternative.get()->serialize();
+    return "if " + condition.get()->serialize() + " " +
+            consequence.get()->serialize() + alt;
+}
 
-    BoolLiteral(Token tok, bool val) : token(tok), value(val){};
-
-    string serialize() const final override {
-        return token.literal;
-    }
-};
-class PrefixExpression : public Expression {
-    public:
-    Token token;
-    string Operator;
-    unique_ptr<Expression> right;
-    
-    PrefixExpression() = default;
-    PrefixExpression(Token tok, string Operator, unique_ptr<Expression>& right) : token(tok), Operator(Operator), right(move(right)) {};
-
-    string serialize() const override {
-        return Operator + right.get()->serialize();
-    }
-};
-class InfixExpression : public Expression {
-    public:
-    Token token;
-    string Operator;
-    unique_ptr<Expression> left;
-    unique_ptr<Expression> right;
-
-    InfixExpression() = default;
-    InfixExpression(Token tok, string Operator, unique_ptr<Expression>& left, unique_ptr<Expression>& right) : token(tok), Operator(Operator), left(move(left)), right(move(right)) {};
-
-    string serialize() const override {
-        // cout << "parsing infix expression" << endl;
-        // cout << "left expression is" << left.get()->serialize() << endl;
-        // cout << "right expression is" << right.get()->serialize() << endl;
-        string res =  "(" + left.get()->serialize() + " " + Operator + " " + right.get()->serialize() + ")";
-        // cout << res << endl;
-        return res;
-    };
-};
 /************************* Statements ************************/
-class Statement : public Node {
-    public:
-    Statement() = default;
-    virtual ~Statement() = default;
-    virtual string getLiteral(){return"";};
-    virtual string serialize() const {return "";};
-    
-};
-class LetStatement: public Statement {
-    public:
-    Token token;
-    Identifier identifier;
-    unique_ptr<Expression> value;
+Statement::Statement() = default;
+Statement::~Statement() = default;
+string Statement::serialize() const {return "";};
 
-    LetStatement() = default;
-    LetStatement(Token tok, Identifier ident, unique_ptr<Expression>& val) : token(tok), identifier(ident), value(move(val)) {};
+LetStatement::LetStatement() = default;
+LetStatement::LetStatement(Token tok, Identifier ident, unique_ptr<Expression>& val) : token(tok), identifier(ident), value(move(val)) {};
+string LetStatement::serialize() const {
+    return token.literal 
+            + " " 
+            + identifier.serialize() 
+            + " = " 
+            + value.get()->serialize() + ";";
     
-    string serialize() const final override {
-        return token.literal 
-                + " " 
-                + identifier.serialize() 
-                + " = " 
-                + value.get()->serialize() + ";";
-        
-    };
 };
-class ReturnStatement: public Statement {
-    public:
-    Token token;
-    unique_ptr<Expression> value;
 
-    ReturnStatement() = default;
-    ReturnStatement(Token tok, unique_ptr<Expression>& val) : token(tok), value(move(val)) {}
-    
-    string serialize() const final override {
-        return token.literal + " " + value.get()->serialize() + ";";
-    }
-};
+ReturnStatement::ReturnStatement() = default;
+ReturnStatement::ReturnStatement(Token tok, unique_ptr<Expression>& val) : token(tok), value(move(val)) {}
+string ReturnStatement::serialize() const {
+    return token.literal + " " + value.get()->serialize() + ";";
+}
 
 /* To allow for a single line expression like "x + 5;"*/
-class ExpressionStatement: public Statement {
-    public:
-    Token token;
-    unique_ptr<Expression> expression;
-
-    ExpressionStatement() {
-        expression = nullptr;
-    };
-
-    ExpressionStatement(Token tok, unique_ptr<Expression>& express): token(tok), expression(move(express)) {};
-
-    string serialize() const final override {
-        cout << "parsing expression statement" << endl;
-        return expression.get()->serialize();
-        // return "";
-    }
+ExpressionStatement::ExpressionStatement() {
+    expression = nullptr;
 };
+ExpressionStatement::ExpressionStatement(Token tok, unique_ptr<Expression>& express): token(tok), expression(move(express)) {};
+string ExpressionStatement::serialize() const {
+    return expression.get()->serialize();
+}
+
+BlockStatement::BlockStatement(Token tok, vector<unique_ptr<Statement>>&& stmts) : token(tok), statements(move(stmts)) {};
+string BlockStatement::serialize() {
+    string res = "{";
+    for (int i = 0; i < statements.size(); i++) {
+        res += statements.at(i).get()->serialize(); + ";";
+    }
+    return res + "}";
+}
 
 /*********************** Program (root node) ********************/ 
-class Program : public Node {
-    public:
-    vector<unique_ptr<Statement>> statements;
-
-    string serialize() const final override {
-        string res = "";
-        for (int i = 0; i < statements.size(); i++) {
-            Statement* stmt = statements.at(i).get();
-            res += stmt->serialize();
-        }
-        return res;
+string Program::serialize() const {
+    string res = "";
+    for (int i = 0; i < statements.size(); i++) {
+        Statement* stmt = statements.at(i).get();
+        res += stmt->serialize();
     }
-};
+    return res;
+}
+
---
+++ ast.h
@@ -0,0 +1,134 @@
+#include<iostream>
+#include"lexer.cpp"
+#include<iostream>
+#include<memory>
+#include<vector>
+
+using namespace std;
+
+class Node {
+    public:
+    virtual string serialize() const = 0;
+};
+
+// turn all fields into pointers
+
+using namespace std;
+
+
+
+/************************* Expressions *********************/
+class Expression : public Node {
+    public:
+    Expression();
+    virtual ~Expression();
+    virtual string serialize() const;
+};
+class Identifier : public Expression {
+    public:
+    Token token;
+    string value;
+    string serialize() const final override;
+};
+/************************* Statements ************************/
+class Statement : public Node {
+    public:
+    Statement();
+    virtual ~Statement();
+    virtual string serialize() const;
+    
+};
+class LetStatement: public Statement {
+    public:
+    Token token;
+    Identifier identifier;
+    unique_ptr<Expression> value;
+    LetStatement();
+    LetStatement(Token tok, Identifier ident, unique_ptr<Expression>& val);
+    
+    string serialize() const final override;
+};
+class ReturnStatement: public Statement {
+    public:
+    Token token;
+    unique_ptr<Expression> value;
+    ReturnStatement();
+    ReturnStatement(Token tok, unique_ptr<Expression>& val);
+    string serialize() const final override;
+};
+
+/* To allow for a single line expression like "x + 5;"*/
+class ExpressionStatement: public Statement {
+    public:
+    Token token;
+    unique_ptr<Expression> expression;
+    ExpressionStatement();
+    ExpressionStatement(Token tok, unique_ptr<Expression>& express);
+
+    string serialize() const final override;
+};
+class BlockStatement : public Statement {
+    public:
+    Token token;
+    vector<unique_ptr<Statement>> statements;
+    BlockStatement(Token tok, vector<unique_ptr<Statement>>&& stmts);
+    string serialize();
+};
+// Expressions
+
+
+class IntLiteral : public Expression {
+    public:
+    Token token;
+    int value;
+    IntLiteral(Token tok, int val);
+    string serialize() const final override;
+};
+class BoolLiteral : public Expression {
+    public:
+    Token token;
+    bool value;
+    BoolLiteral(Token tok, bool val);
+    string serialize() const final override;
+};
+class PrefixExpression : public Expression {
+    public:
+    Token token;
+    string Operator;
+    unique_ptr<Expression> right;
+    PrefixExpression();
+    PrefixExpression(Token tok, string Operator, unique_ptr<Expression>& right);
+    string serialize() const override;
+};
+class InfixExpression : public Expression {
+    public:
+    Token token;
+    string Operator;
+    unique_ptr<Expression> left;
+    unique_ptr<Expression> right;
+    InfixExpression();
+    InfixExpression(Token tok, string Operator, unique_ptr<Expression>& left, unique_ptr<Expression>& right);
+    string serialize() const override;
+};
+class IfExpression : public Expression {
+    public:
+    Token token;
+    unique_ptr<Expression> condition;
+    unique_ptr<BlockStatement> consequence;
+    unique_ptr<BlockStatement> alternative;
+
+    IfExpression(Token tok, 
+    unique_ptr<Expression>& cond, 
+    unique_ptr<BlockStatement>& cons, 
+    unique_ptr<BlockStatement>& alt);
+    
+    string serialize() const override;
+};
+
+
+/*********************** Program (root node) ********************/ 
+class Program : public Node {
+    public:
+    vector<unique_ptr<Statement>> statements;
+    string serialize() const final override;
+};
