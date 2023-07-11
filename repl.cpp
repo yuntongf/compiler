84c2b857050c39cbdac26a08871a4231f011dfdf
August Fu <yuntongf@gmail.com>
2023-07-11 14:37:28 -0400

    test call-function

--- repl.cpp
+++ repl.cpp
@@ -1,9 +1,9 @@
-#include"lexer.cpp"
+#include"parser.cpp"
 #include<iostream>
 
 using namespace std;
 
-#define MAX_INPUT_LENGTH 100
+#define MAX_INPUT_LENGTH 200
 
 void repl() {
     while (1) {
@@ -12,9 +12,13 @@ void repl() {
         cin.getline(input, MAX_INPUT_LENGTH, '\n');
 
         Lexer l = Lexer(input);
-        for (Token tok = l.nextToken(); tok.type != types.EoF; tok = l.nextToken()) {
-            cout << "{Type: " << tok.type
-                 << " Literal: " << tok.literal << "}" << endl;
+        Parser p = Parser(l);
+        auto program = Program();
+        int error = p.parseProgram(&program);
+        if (error == 0) {
+            for (int i = 0; i < program.statements.size(); i++) {
+                cout << program.statements.at(i).get()->serialize() << endl;
+            }
         }
         // cin.clear();
     }
